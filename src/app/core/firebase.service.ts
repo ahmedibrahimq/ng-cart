fe0c792483c58fdbaca40ceefe012e89852cae60
ahmedibrahimq <ahmedimiq@gmail.com>
2020-06-22 22:50:08 +0200

    add crud operations for user orders

--- src/app/core/firebase.service.ts
+++ src/app/core/firebase.service.ts
@@ -2,11 +2,9 @@ import { Injectable } from '@angular/core';
 import { AngularFireDatabase } from '@angular/fire/database/';
 import { Observable } from 'rxjs';
 import { map, reduce } from 'rxjs/operators';
-import { keyframes } from '@angular/animations';
-
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FirebaseService {
   cartsUrl = '/carts';
@@ -21,55 +19,100 @@ export class FirebaseService {
   }
 
   getProducts(): Observable<any[]> {
-    return this.db.list(this.productsUrl).snapshotChanges().pipe(
-      map(changes => changes.map(
-        change => ({ key: change.payload.key, ...(change.payload.val() as {}) })
+    return this.db
+      .list(this.productsUrl)
+      .snapshotChanges()
+      .pipe(
+        map((changes) =>
+          changes.map((change) => ({
+            key: change.payload.key,
+            ...(change.payload.val() as {}),
+          }))
         )
-      )
-    );
+      );
   }
 
   getCategories(): Observable<any[]> {
     return this.db.list(this.categoriesUrl).valueChanges();
   }
 
-  getCartKey() { return localStorage.getItem('cartKey'); }
-
-  getCart(): Observable<any[]> {
-    return this.db.list(`${this.cartsUrl}/${this.getCartKey()}`).snapshotChanges().pipe(
-      map(changes => changes.map(
-        change => ({ key: change.payload.key, ...(change.payload.val() as {}) })
-        )
-      )
-    );
+  getCartKey() {
+    return localStorage.getItem('cartKey');
   }
 
-  getCartCount(): Observable<number> {
-    return this.getCart()
-      .pipe(map(cart =>
-        cart
-          .map((item: any) => item.quantity)
-          .reduce((acc, curr) => acc + curr, 0)
+  getCart(): Observable<any[]> {
+    return this.db
+      .list(`${this.cartsUrl}/${this.getCartKey()}`)
+      .snapshotChanges()
+      .pipe(
+        map((changes) =>
+          changes.map((change) => ({
+            key: change.payload.key,
+            ...(change.payload.val() as {}),
+          }))
         )
       );
   }
 
+  clearCart() {
+    return this.db.list(`${this.cartsUrl}/${this.getCartKey()}`).remove();
+  }
+
+  getOrders() {
+    return this.db
+      .list(`${this.ordersUrl}/${this.getCartKey()}`)
+      .snapshotChanges()
+      .pipe(
+        map((changes) =>
+          changes.map((change) => ({
+            key: change.payload.key,
+            items: change.payload.val(),
+          }))
+        )
+      );
+  }
+
+  addOrder(order) {
+    // console.log(this.ordersUrl, this.getCartKey(), 'order');
+    this.db.database
+      .ref(this.ordersUrl)
+      .child(this.getCartKey())
+      .update({ [new Date().toLocaleString().replace(/\//g, '-')]: order });
+  }
+
+  getCartCount(): Observable<number> {
+    return this.getCart().pipe(
+      map((cart) =>
+        cart
+          .map((item: any) => item.quantity)
+          .reduce((acc, curr) => acc + curr, 0)
+      )
+    );
+  }
+
   getCartItem(key): Observable<any> {
-    return this.db.object(`${this.cartsUrl}/${this.getCartKey()}/${key}`).valueChanges();
+    return this.db
+      .object(`${this.cartsUrl}/${this.getCartKey()}/${key}`)
+      .valueChanges();
   }
 
   getCartItemCount(key): Observable<number> {
-    return this.getCartItem(key)
-      .pipe(map(item => item ? item.quantity : 0));
+    return this.getCartItem(key).pipe(
+      map((item) => (item ? item.quantity : 0))
+    );
   }
 
   AddCartItem(item) {
-    const data = {title: item.title, unitPrice: item.price, quantity: 1};
+    const data = { title: item.title, unitPrice: item.price, quantity: 1 };
     return this.updateCartItem(item.key, data);
   }
 
   updateCartItem(itemKey, value) {
-    this.db.database.ref(this.cartsUrl).child(this.getCartKey()).child(itemKey).update(value);
+    this.db.database
+      .ref(this.cartsUrl)
+      .child(this.getCartKey())
+      .child(itemKey)
+      .update(value);
   }
 
   removeCartItem(key: string) {
