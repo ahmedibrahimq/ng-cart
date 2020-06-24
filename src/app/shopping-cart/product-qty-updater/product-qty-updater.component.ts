0066dbf2bcbb4207c50bda3550d885d3e22db32f
ahmedibrahimq <ahmedimiq@gmail.com>
2020-06-24 14:00:09 +0200

    add remove-from-cart button to the shopping cart

--- src/app/shopping-cart/product-qty-updater/product-qty-updater.component.ts
+++ src/app/shopping-cart/product-qty-updater/product-qty-updater.component.ts
@@ -4,29 +4,35 @@ import { FirebaseService } from 'src/app/core/firebase.service';
 @Component({
   selector: 'app-product-qty-updater',
   templateUrl: './product-qty-updater.component.html',
-  styleUrls: ['./product-qty-updater.component.scss']
+  styleUrls: ['./product-qty-updater.component.scss'],
 })
 export class ProductQtyUpdaterComponent implements OnInit {
-
   @Input() itemKey: string;
+  @Input() quantitative: boolean;
 
   countInCart: number;
 
-  constructor(private firebase: FirebaseService) { }
+  constructor(private firebase: FirebaseService) {}
 
   inc() {
-    this.firebase.updateCartItem(this.itemKey, {quantity: this.countInCart + 1});
+    this.firebase.updateCartItem(this.itemKey, {
+      quantity: this.countInCart + 1,
+    });
   }
 
   dec() {
-    if (this.countInCart <= 1) { return this.firebase.removeCartItem(this.itemKey); }
+    if (this.countInCart <= 1) {
+      return this.firebase.removeCartItem(this.itemKey);
+    }
 
-    this.firebase.updateCartItem(this.itemKey, {quantity: this.countInCart - 1});
+    this.firebase.updateCartItem(this.itemKey, {
+      quantity: this.countInCart - 1,
+    });
   }
 
   ngOnInit(): void {
-    this.firebase.getCartItemCount(this.itemKey).subscribe(value => this.countInCart = value);
-
+    this.firebase
+      .getCartItemCount(this.itemKey)
+      .subscribe((value) => (this.countInCart = value));
   }
-
 }
